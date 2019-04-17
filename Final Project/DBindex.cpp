ee0df607c6e75922d8bf23633d36a8eaa269879f
Gentry Atkinson <gma23@txstate.edu>
2019-04-17 12:13:35 -0500

    generalized DBindex to high d and added clock

--- Final Project/DBindex.cpp
+++ Final Project/DBindex.cpp
@@ -3,54 +3,62 @@
 #include <stdio.h>
 #include <cmath>
 #include <limits>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 struct point{
-	float x;
-	float y;
 	int cluster;
+	float* values;
 };
 
-float euDis(point, point);
-float calcS(point*, point, int);
+float euDis(point, point, int);
+float calcS(const point*, point, int, int);
 
 int main(int argc, char** argv){
-	if (argc != 4) {
-		cerr << "Command format is \"DBindex num_points num_clusters filename\"" << endl;
+		if (argc != 5) {
+		cerr << "Command format is \"DBIndex num_points num_clusters dimensions filename\"" << endl;
 		return 1;
 	}
 
 	numeric_limits<float> bound;
-	int numPoints = atoi(argv[1]);
-	int numClusters = atoi(argv[2]);
-	ifstream inFile(argv[3]);
+	const int numPoints = atoi(argv[1]);
+	const int numClusters = atoi(argv[2]);
+	const int dimensions = atoi(argv[3]);
+	ifstream inFile(argv[4]);
 
 	point points[numPoints];
 	point centers[numClusters];
 	int temp = 0;
 	while (!inFile.eof()){
-        inFile >> points[temp].x >> points[temp].y >> points[temp].cluster;
+        points[temp].values = new float[dimensions];
+        for (int i = 0; i < dimensions; ++i){
+            inFile >> points[temp].values[i];
+        }
+        inFile >> points[temp].cluster;
         temp++;
 	}
+	auto start = high_resolution_clock::now();
 
 	//calculate centers
-	float xSum[numClusters];
-    float ySum[numClusters];
+    float sums[numClusters][dimensions];
     float totalPoints[numClusters];
-	for(int i = 0; i < numClusters; i++){
-        xSum[i] = 0;
-        ySum[i] = 0;
+	for(int i = 0; i < numClusters; ++i){   //initialize everything to 0
+        for(int j = 0; j < dimensions; ++j)
+            sums[i][j] = 0;
         totalPoints[i] = 0;
 	}
-	for (int i = 0; i < numPoints; i++){
-        xSum[points[i].cluster-1] += points[i].x;
-        ySum[points[i].cluster-1] += points[i].y;
+	for (int i = 0; i < numPoints; ++i){    //sum across all dimensions of the point list
+        for (int j = 0; j < dimensions; ++j)
+            sums[points[i].cluster-1][j] += points[i].values[j];
         totalPoints[points[i].cluster-1] += 1;
 	}
-	for (int i = 0; i < numClusters; i++){
-        centers[i].x = (xSum[i] / totalPoints[i]);
-        centers[i].y = (ySum[i] / totalPoints[i]);
+	for (int i = 0; i < numClusters; ++i){ //divide sum by points to get centers
+        centers[i].values = new float[dimensions];
+        for (int j = 0; j < dimensions; ++j){
+            centers[i].values[j] = (sums[i][j] / totalPoints[i]);
+        }
         centers[i].cluster = i+1;
 	}
 
@@ -60,30 +68,37 @@ int main(int argc, char** argv){
 	for (int i = 0; i < numClusters-1; i++){
         D[i] = 0;
         for (int j = i+1; j < numClusters; j++){
-            float S = (calcS(points, centers[i], numPoints) + calcS(points, centers[j], numPoints)) / euDis(centers[i], centers[j]);
+            float S = (calcS(points, centers[i], numPoints, dimensions) + calcS(points, centers[j], numPoints, dimensions)) / euDis(centers[i], centers[j], dimensions);
             if (S > D[i])
                 D[i] = S;
         }
         totalD += D[i];
 	}
 
-	cout << totalD / numClusters << endl;
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+
+	cout << "Davies-Boudlin Index: " << totalD / numClusters << endl;
+	cout << "Time to run: " << (duration.count())/1000.0 << " milliseconds." << endl;
 
 	return 0;
 }
 
-float euDis(point p, point c){
-	float dist = pow((p.x - c.x), 2) + pow((p.y - c.y), 2);
+float euDis(point p, point c, int d){
+	float dist = 0;
+	for (int i = 0; i < d; ++i){
+        dist += ((p.values[i] - c.values[i]) * (p.values[i] - c.values[i]));
+	}
 	dist = sqrt(dist);
 	return dist;
 }
 
-float calcS(point* points, point center, int numPoints){
+float calcS(const point* points, point center, int numPoints, int d){
     int numClusterPoints = 0;
     float totalDis = 0;
     for (int i = 0; i < numPoints; i++){
         if (points[i].cluster == center.cluster){
-            totalDis += euDis(points[i], center);
+            totalDis += euDis(points[i], center, d);
             numClusterPoints += 1;
         }
     }
